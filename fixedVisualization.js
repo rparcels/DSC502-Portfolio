61119e17438eed34937f085c547bbbed27fdb36d
rparcels <rparcels@umich.edu>
2026-09-24 22:26:11 -0400

    Update fixedVisualization.js

--- fixedVisualization.js
+++ fixedVisualization.js
@@ -133,7 +133,6 @@ const fixedVisualizationSpec = {
       }
     }
   ],
-  "title": "Typical Caffeine Content by Beverage",
-  "width: 600
+  "title": "Typical Caffeine Content by Beverage"
 }
 vegaEmbed('#fixedVisualization', fixedVisualizationSpec, { actions: false });
